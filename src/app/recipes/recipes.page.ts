825dc7ed0454e685a69b6b7cb5261938c415f918
Chia Che Chang <midtic0404@gmail.com>
2019-10-25 00:06:09 -0600

    1025 update

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
 
@@ -7,13 +7,41 @@ import { RecipesService } from './recipes.service';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
 
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+
+    console.log('Loaded!');
+    
+  }
+
+  ionViewWillEnter() {
+    console.log('will enter');
     this.recipes = this.recipesService.getAllRecipes();
+    
+  }
+
+  ionViewWillLeave() {
+    console.log('will leave');
+    
+  }
+
+  ionViewDidLeave() {
+    console.log('did leave');
+    
+  }
+
+  ionViewDidEnter() {
+    console.log('did enter');
+    
+  }
+
+  ngOnDestroy() {
+    console.log('destroyed!');
+    
   }
 
 }
